cb63f08267d68459d5c5ae446d6bec5a66a8dcfe
Fayas pn <fayaspn143@gmail.com>
2023-04-02 14:38:32 +0530

    nodejs server created

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 4. Create Git Repository
 5. Adding lists
 6. Add routing
+7. Create a Node.js Server
---
+++ backend/data.js
@@ -0,0 +1,50 @@
+const data = {
+    products: [
+        {
+            name:'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/shirt-1.jpg',
+            price:120,
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt'
+        },
+        {
+            name:'Adidas fit Shirt',
+            slug: 'nike-slim-shirt',
+            category:'Shirts',
+            image:"/images/shirt-2.jpg",
+            price:120,
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt'
+        },
+        {
+            name:'Nike Slim pant',
+            slug: 'nike-slim-shirt',
+            category:'Shirts',
+            image:"/images/pant-1.jpg",
+            price:120,
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt'
+        },
+        {
+            name:'Adidas Slim pant',
+            slug: 'nike-slim-shirt',
+            category:'Shirts',
+            image:"/images/pant-2.jpg",
+            price:120,
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'high quality shirt'
+        },
+    ],
+};
+
+export default data;
